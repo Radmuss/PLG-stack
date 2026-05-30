e07c188bf6b43ca26392fbe8cb60aa9e107cedd3
BigBadSheep <283820550+MieszkoMerito@users.noreply.github.com>
2026-05-31 01:43:30 +0200

    inicjacja plików
    inicjacja plików

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
+  }
+}
+provider "azurerm" { features {} }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "RG-Observability-Lab"
+  location = "westeurope"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-observability"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksobserv"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v3"
+  }
+  identity { type = "SystemAssigned" }
+}
